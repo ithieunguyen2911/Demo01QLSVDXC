7a338c4b64768e2c667bcc6ed5ecd371081f97a0
ithieunguyen2911 <usahieunguyen2018@gmail.com>
2021-03-03 11:33:14 +0700

    push code demo QLSV DXC first time

---
+++ Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo01_QLSV_DXC
+{
+    public class Person
+    {
+        //Declare Variable
+        private string fullName;
+        private DateTime dateOfBirth;
+        private bool gender;
+        private string address;
+
+        //Constructor
+        public Person()
+        {
+
+        }
+        public Person(string FullName, DateTime DateOfBirth, bool Gender, string Address)
+        {
+            this.FullName = FullName;
+            this.DateOfBirth = DateOfBirth;
+            this.Gender = Gender;
+            this.Address = Address;
+        }
+
+        //Getter & Setter
+        public string FullName { get => fullName; set => fullName = value; }
+        public DateTime DateOfBirth { get => dateOfBirth; set => dateOfBirth = value; }
+        public bool Gender { get => gender; set => gender = value; }
+        public string Address { get => address; set => address = value; }
+
+        //ToString() : purpose to print infomation.
+        public override string ToString()
+        {
+            return "Ho va Ten : " + FullName + "Ngay sinh : " + DateOfBirth + "Gioi tinh : " + Gender + "Dia Chi : " + Address;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Demo01_QLSV_DXC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
